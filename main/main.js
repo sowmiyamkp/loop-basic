46669bbb2ac440f9fe638ee2291da64bdd09b0e0
sowmiyamkp <sowmiyamkp1305@gmail.com>
2023-03-25 21:46:31 -0700

    add loop-basic

---
+++ main/main.js
@@ -0,0 +1,29 @@
+
+	                //Loop
+	
+						//while
+	
+ var i=3;
+ while(i<=6)
+{
+	 console.log("true");
+	 i++;
+ }
+ 
+						//do while
+ 
+ 
+ var x=6
+  do{
+	 console.log("Hello world");
+	 	 x++;
+ }
+ while(x<=9)
+
+                        //for loop
+var b=2
+ for (b=2;b<=7;b++)
+ {
+	 console.log("true");
+ }
+ 
